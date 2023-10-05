34a7a3b9d60212891df1a8ce5d283b2bb66820cb
vuro77 <vilmabostrom23@gmail.com>
2023-10-05 15:16:56 +0200

    Pheromone/Implementation WIP
    diffusion and neighbour check done

--- Objects/phermone.py
+++
@@ -1,9 +0,0 @@
-from pygame import Vector2
-from ECS.world import WorldObject
-
-
-class Phermone(WorldObject):
-
-    def __init__(self, position: Vector2):
-        self.position = position
-        self.radius = 2
