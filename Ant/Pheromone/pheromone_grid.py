5e7d23127eb2978cb99746e07154b07a10695713
vuro77 <vilmabostrom23@gmail.com>
2023-10-07 19:44:51 +0200

    Optimizing go woosh
    doesn't do all calculations twice twice but just once now
    and removed a lot of unnecessary stuff

--- Ant/Pheromone/pheromone_grid.py
+++ Ant/Pheromone/pheromone_grid.py
@@ -7,10 +7,10 @@ from pygame import Color, Surface, Vector2
 import pygame
 from Util.globals import SCREEN_SIZE
 
-TIME: Final = 0.4
+TIME: Final = 1
 DIFFUSION_EDGE: Final = 10
 DIFFUSION_MIDDLE: Final = 100
-CELL_SIZE: Final = 40
+CELL_SIZE: Final = 20
 MAX_PER_TILE: Final = 255
 
 
@@ -40,7 +40,7 @@ class PheromoneGrid:
 
         if self.timer >= TIME:
             self.timer = self.timer % TIME
-            self.grid_list = get_diffused_list(self.grid_list)
+            self.grid_list = generate_diffused_list(self.grid_list, generate_diffusion_amount(self.grid_list))
 
     def add(self,
             position: Vector2,
@@ -76,100 +76,47 @@ class PheromoneGrid:
         return sum(self.grid_list)
 
 
-def get_diffused_list(grid_list: list[int], grid_size: tuple[int, int] = GRID_SIZE) -> list[int]:
-    new_grid_list: list[int] = []
+def generate_diffusion_amount(grid_list: list[int], grid_size: tuple[int, int] = GRID_SIZE):
+
+    grid_diffusion_amount: tuple[int, ...] = ()
 
     for i in range(prod(grid_size)):
-        s: int = grid_list[i]
+        not_top_or_bottom: bool = not (
+            i < grid_size[0] or i > grid_size[0]*(grid_size[1]-2))
+        
+        not_left_or_right: bool = not (
+            i % grid_size[0] == 0 or (i+1) % grid_size[0] == 0)
+        
+        grid_diffusion_amount += ((DIFFUSION_EDGE * grid_list[i])
+                                  // DIFFUSION_STRENGTH_MAP[not_top_or_bottom + not_left_or_right],)
+        
+    return grid_diffusion_amount
 
-        case: int = get_map_case(i)
 
-        if (i >= grid_size[0]):
-            """up"""
-            s += diffusion_calc(grid_list, i, i - grid_size[0], case)
+def generate_diffused_list(grid_list: list[int], grid_diffusion_amount: tuple[int, ...],  grid_size: tuple[int, int] = GRID_SIZE):
+    new_grid_list: list[int] = []
+    for i in range(prod(grid_size)):
 
-        if ((i+1) % grid_size[0]):
-            """right"""
-            s += diffusion_calc(grid_list, i, i + 1, case)
+        top: bool = i < grid_size[0]
+        bottom: bool = i > grid_size[0]*(grid_size[1]-2)
+        left: bool = i % grid_size[0] == 0
+        right: bool = (i+1) % grid_size[0] == 0
 
-        if (i <= grid_size[0]*(grid_size[1]-2)):
-            """down"""
-            s += diffusion_calc(grid_list, i, i + grid_size[0], case)
+        s: int = grid_list[i] - (4 - (top+bottom+left+right)
+                                 ) * grid_diffusion_amount[i]
 
-        if (i % grid_size[0]):
-            """left"""
-            s += diffusion_calc(grid_list, i, i - 1, case)
+        if not (top):
+            s += grid_diffusion_amount[i - grid_size[0]]
 
-        new_grid_list.append(min(s-(s >> 5), MAX_PER_TILE))
+        if not (right):
+            s += grid_diffusion_amount[i + 1]
+
+        if not (bottom):
+            s += grid_diffusion_amount[i + grid_size[0]]
+
+        if not (left):
+            s += grid_diffusion_amount[i - 1]
+
+        new_grid_list.append(min(s-(s >> 4), MAX_PER_TILE))
 
     return new_grid_list
-
-
-def diffusion_calc(grid_list: list[int], index: int, neighbour_index: int, case: int) -> int:
-    return ((DIFFUSION_EDGE * grid_list[neighbour_index])
-            // DIFFUSION_STRENGTH_MAP[get_map_case(neighbour_index)]
-            -
-            (DIFFUSION_EDGE * grid_list[index])
-            // DIFFUSION_STRENGTH_MAP[case])
-
-
-def get_map_case(index: int, grid_size: tuple[int, int] = GRID_SIZE) -> int:
-    top: bool = index < grid_size[0]
-    bottom: bool = index > grid_size[0]*(grid_size[1]-2)
-    left: bool = index % grid_size[0] == 0
-    right: bool = (index+1) % grid_size[0] == 0
-
-    match (left, top, right, bottom):
-        case True, True, False, False:
-            """Top_Left"""
-            return 0
-
-        case False, True, False, False:
-            """"Top_Middle"""
-            return 1
-
-        case False, True, True, False:
-            """"Top_Right"""
-            return 0
-
-        case True, False, False, False:
-            """"Middle_Left"""
-            return 1
-
-        case False, False, False, False:
-            """"Middle_Middle"""
-            return 2
-
-        case False, False, True, False:
-            """"Middle_Right"""
-            return 1
-
-        case True, False, False, True:
-            """"Bottom_Left"""
-            return 0
-
-        case False, False, False, True:
-            """"Bottom_Middle"""
-            return 1
-
-        case False, False, True, True:
-            """"Bottom_Right"""
-            return 0
-
-        case _:
-            raise ValueError
-
-
-def case_selection():
-    num = 1
-    for x in range(9):
-        yield x, num
-        num <<= 1
-
-# pg = PheromoneGrid((10, 10))
-# pg.grid_list[25] = 10
-# pg.grid_list[80] = 10
-# print(pg)
-# for _ in range(1):
-    # pg.update(TIME)
-    # print(pg)
