ee037a27c6e29c7b6694aab9ac105077187bbe23
vuro77 <vilmabostrom23@gmail.com>
2023-10-12 15:15:41 +0200

    Chunking for pheromone grid hopefully done
    tested for most cases

--- Ant/Pheromone/grid_chunking.py
+++ Ant/Pheromone/grid_chunking.py
@@ -3,11 +3,14 @@ from dataclasses import dataclass
 from math import prod
 from typing import Any, Final, Generator
 
-from Ant.Pheromone.pheromone_grid import GRID_SIZE
+#from Ant.Pheromone.pheromone_grid import  GRID_SIZE
+
+GRID_SIZE = (16*4,9*4)
 
 CHUNK_SIZE: Final = 4
 
 CHUNK_GRID_SIZE: Final = (GRID_SIZE[0]//CHUNK_SIZE, GRID_SIZE[1]//CHUNK_SIZE)
+CHUNK_GRID_LEN: Final = prod(CHUNK_GRID_SIZE)
 
 
 def generate_chunk_index_tuple(
@@ -26,11 +29,13 @@ CHUNK_INDEX_TUPLE: Final = generate_chunk_index_tuple()
 
 
 def get_grid_indexes_from_chunk(
-        chunk_start_index: int,
+        chunk_index: int,
+        chunk_indexes:tuple[int,...] = CHUNK_INDEX_TUPLE,
         chunk_size: int = CHUNK_SIZE,
         grid_size: tuple[int, int] = GRID_SIZE
 ) -> Generator[int, Any, None]:
 
+    chunk_start_index: int = chunk_indexes[chunk_index]
     for i in range(chunk_size**2):
         yield chunk_start_index + i % chunk_size + grid_size[0]*(i//chunk_size)
 
@@ -42,10 +47,10 @@ def generate_hex_map_cases(
 
     hex_map: int = 0
     for i in range(prod(chunk_grid_size)):
-        not_top: bool = i >= grid_size[0]
-        not_bottom: bool = i <= grid_size[0]*(grid_size[1]-2)
-        not_left: bool = i % grid_size[0] != 0
-        not_right: bool = (i+1) % grid_size[0] != 0
+        not_top: bool = i >= chunk_grid_size[0]
+        not_bottom: bool = i <= chunk_grid_size[0]*(chunk_grid_size[1]-1)
+        not_left: bool = i % chunk_grid_size[0] != 0
+        not_right: bool = (i+1) % chunk_grid_size[0] != 0
 
         hex: int = not_top << 3 | not_bottom << 2 | not_left << 1 | not_right
 
@@ -64,31 +69,92 @@ class Chunk_Grid:
         self.bit_map = 0
 
     def add(self, index: int):
-        self.bit_map |= index_to_active(index)
+        self.bit_map |= (1 << (index << 1))
 
         self.turn_on_lazy_around_index(index)
+
+    def flip(self, index:int):
+        self.bit_map ^= (1 << (index << 1))
+
+        self.uppdate_lazy_around_index(index)
+
+    def remove(self, index:int):
+        self.bit_map ^= (self.bit_map & (1 << (index << 1)))
+
+        self.uppdate_lazy_around_index(index)
 
     def any_neighbour_active(
             self,
             index: int,
+            chunk_grid_len: int = CHUNK_GRID_LEN
     ):
 
-        return bool(self.bit_map & (neighbour_indexes(index,0)))
+        return bool(
+            0 <= index
+            and index < chunk_grid_len
+            and (self.bit_map & (neighbour_indexes(index, 0))))
 
     def uppdate_lazy_around_index(
             self,
-            index: int
+            index: int,
+            chunk_grid_size: tuple[int, int] = CHUNK_GRID_SIZE
     ):
+        # index for chunk above (index)
+        top_index = index - chunk_grid_size[0]
+        # index for chunk bellow (index)
+        bottom_index = index + chunk_grid_size[0]
+        # index for chunk to the left of (index)
+        left_index = index-1
+        # index for chunk to the right of (index)
+        right_index = index+1
 
-        top = self.any_neighbour_active(top_(index)) << 1+(top_(index) << 1)
-        bottom = self.any_neighbour_active(
-            bottom_(index)) << 1+(bottom_(index) << 1)
-        left = self.any_neighbour_active(left_(index)) << 1+(left_(index) << 1)
-        right = self.any_neighbour_active(
-            right_(index)) << 1+(right_(index) << 1)
-        middle = self.any_neighbour_active(index) << 1+(index << 1)
+        top = (
+            # bool if there is a chunk active around the chunk above
+            self.any_neighbour_active(top_index)
+            # shifting the bool to the top index
+            << 1+(
+                max(0,top_index)<< 1
+            )
+        )
 
-        self.bit_map ^= (self.bit_map & neighbour_indexes(index,1)) ^ (
+        bottom = (
+            # bool if there is a chunk active around the chunk bellow
+            self.any_neighbour_active(bottom_index)
+            # shifting the bool to the bottom index
+            << 1+(
+                bottom_index<< 1
+            )
+        )
+
+        left = (
+            # bool if there is a chunk active around the chunk to the left
+            self.any_neighbour_active(left_index)
+            # shifting the bool to the left index
+            << 1+(
+                max(0,left_index)<< 1
+            )
+        )
+
+        right = (
+            # bool if there is a chunk active around the chunk to the right
+            self.any_neighbour_active(right_index)
+            # shifting the bool to the right index
+            << 1+(
+                right_index<< 1
+            )
+        )
+
+        middle = (
+            # bool if there is a chunk active around the chunk
+            self.any_neighbour_active(index)
+            # shifting the bool to the index
+            << 1+(
+                index<< 1
+            )
+        )
+
+        self.bit_map ^= (
+            self.bit_map & neighbour_indexes(index, 1)) ^ (
             top | bottom | left | right | middle)
 
     def turn_on_lazy_around_index(
@@ -96,95 +162,80 @@ class Chunk_Grid:
             index: int
     ):
 
-        self.bit_map |= neighbour_indexes(index,1)
+        self.bit_map |= neighbour_indexes(index, 1)
 
+    def __str__(
+            self,
+            chunk_grid_len:int = CHUNK_GRID_LEN,
+            chunk_grid_size: tuple[int,int] = CHUNK_GRID_SIZE
+            ):
+        
+        s = ""
+        for i in range(chunk_grid_len):
+            x = 3&(self.bit_map >>(i<<1))
+            b = (chunk_grid_size[0]-i-1) % chunk_grid_size[0] == 0
+            s += f"{x: <7}" + 2 * "\n" * b
+        return s
 
 def neighbour_indexes(
-        index: int,
-        offset: int, #offset for lazy or active
+        chunk_index: int,
+        intra_chunk_offset: int,  # offset for lazy or active
         chunk_grid_size: tuple[int, int] = CHUNK_GRID_SIZE,
+        hex_map: int = HEX_MAP_CASES,
 ):
 
+
     top = (
-        get_hex_from_map(index, 3) #bool if there is a chunk above
-        << offset + ( 
-            (max(0, index - chunk_grid_size[0])) #index for chunk above (index)
+        # bool if there is a chunk above
+        (1 & (hex_map >> ((chunk_index << 2)+3)))
+        << intra_chunk_offset + (
+            # index for chunk above (index)
+            (max(0, chunk_index - chunk_grid_size[0]))
             << 1
         )
     )
 
     bottom = (
-        get_hex_from_map(index, 2) #bool if there is a chunk bellow
-        << offset + (
-            (index + chunk_grid_size[0]) #index for chunk bellow (index)
+        # bool if there is a chunk bellow
+        (1 & (hex_map >> ((chunk_index << 2)+2)))
+        << intra_chunk_offset + (
+            # index for chunk bellow (index)
+            (chunk_index + chunk_grid_size[0])
             << 1
         )
     )
 
     left = (
-        get_hex_from_map(index, 1) #bool if there is a chunk to the left
-        << offset + (
-            (max(0, index - 1)) #index for chunk to the left of (index)
+        # bool if there is a chunk to the left
+        (1 & (hex_map >> ((chunk_index << 2)+1)))
+        << intra_chunk_offset + (
+            # index for chunk to the left of (index)
+            (max(0, chunk_index - 1))
             << 1
         )
     )
 
     right = (
-        get_hex_from_map(index, 0) #bool if there is a chunk to the right
-        << offset + (
-            (index + 1) #index for chunk to the right of (index)
+        # bool if there is a chunk to the right
+        (1 & (hex_map >> ((chunk_index << 2))))
+        << intra_chunk_offset + (
+            # index for chunk to the right of (index)
+            (chunk_index + 1)
             << 1
         )
     )
 
     middle = (
+        # bool if there is a chunk at the start index (which it better be)
         1
-        << offset + (
-            (index)
+        << intra_chunk_offset + (
+            # index for chunk
+            chunk_index
             << 1
         )
     )
 
+    # returns a bitmap of all neighbouring chunks scaled to be seamless with the larger bitmap of all the chunks
     return top | bottom | left | right | middle
 
 
-def top_(
-        index: int,
-        chunk_grid_size: tuple[int, int] = CHUNK_GRID_SIZE
-) -> int:
-    return max(0, index - chunk_grid_size[0])
-
-
-def bottom_(
-        index: int,
-        chunk_grid_size: tuple[int, int] = CHUNK_GRID_SIZE
-) -> int:
-    return index + chunk_grid_size[0]
-
-
-def left_(
-        index: int
-) -> int:
-    return max(0, index - 1)
-
-
-def right_(
-        index: int
-) -> int:
-    return index + 1
-
-
-def get_hex_from_map(
-        index: int,
-        offset: int = 0,
-        hex_map: int = HEX_MAP_CASES):
-
-    return 1 & (hex_map >> (index << 2 + offset))
-
-
-def index_to_active(index: int):
-    return 1 << (index << 1)
-
-
-def index_to_lazy(index: int):
-    return 1 << 1+(index << 1)
--- Ant/Pheromone/test.py
+++ Ant/Pheromone/test.py
@@ -1,5 +1,32 @@
 
 
+from grid_chunking import Chunk_Grid
 
+chunks = Chunk_Grid()
 
-print(1<<(-3<<1))
+print(chunks)
+
+chunks.add(2)
+
+print(chunks)
+
+chunks.flip(20)
+chunks.flip(21)
+chunks.flip(22)
+chunks.flip(36)
+chunks.flip(37)
+chunks.flip(38)
+chunks.flip(52)
+chunks.flip(53)
+chunks.flip(54)
+
+print(chunks)
+
+chunks.flip(20)
+chunks.flip(22)
+chunks.remove(37)
+chunks.flip(52)
+chunks.flip(54)
+
+print(chunks)
+
