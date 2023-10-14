bd4598875cae0056f5c263ef321b4fa82c2d9c31
G433N <44841986+G433N@users.noreply.github.com>
2023-10-14 12:41:23 +0200

    Update test.py

--- Ant/Pheromone/test.py
+++ Ant/Pheromone/test.py
@@ -1,18 +1,14 @@
 
 
-
 from typing import Any
-import numpy
-np = numpy
-shape = (3,5)
+import numpy as np
+shape = (3, 5)
 x = 3
-m: np.ndarray[int, np.dtype[Any]] = np.zeros(shape,int)
+m: np.ndarray[int, np.dtype[Any]] = np.zeros(shape, int)
 
 
 m[2][1] += 8
 m[1][3] += 43
-
-
 
 
 def diffusion(arr: np.ndarray[int, np.dtype[Any]]):
@@ -31,6 +27,7 @@ def diffusion(arr: np.ndarray[int, np.dtype[Any]]):
 def diffusion_stack(arr: np.ndarray[int, np.dtype[Any]]):
 
     return np.vstack((arr[0], arr, arr[-1]))
+
 
 def diffused_array(arr: np.ndarray[int, np.dtype[Any]]):
     return arr + diffusion(arr//6)
